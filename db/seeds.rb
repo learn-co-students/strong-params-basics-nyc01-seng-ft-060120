ec368b01fbdb1344df55634500d4ff49d764dd47
alexpac27 <alexpac27@gmail.com>
2020-06-25 20:42:38 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title:"eat my butt", description: "How to make the perfect Christmas Ham")
